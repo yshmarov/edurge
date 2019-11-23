c096b9f5e977ea6c10a490e1731c5b1243c89333
Yaro <yshmarov@gmail.com>
2019-11-23 20:25:10 +0000

    fully functional sidebar with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def username
+    if email.present?
+      self.email.split(/@/).first
+    end
+  end
 end
