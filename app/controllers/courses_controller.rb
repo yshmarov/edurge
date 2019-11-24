43ed5c349bbea686656d30d2f57b05b7d8c8b8ce
Yaro <yshmarov@gmail.com>
2019-11-24 00:51:39 +0000

    tasks inside courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -10,6 +10,7 @@ class CoursesController < ApplicationController
   # GET /courses/1
   # GET /courses/1.json
   def show
+    @tasks = @course.tasks.order(chapter: :asc)
   end
 
   # GET /courses/new
--- app/views/tasks/_task.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! task, :id, :name, :descroption, :video_url, :chapter_id, :created_at, :updated_at
-json.url task_url(task, format: :json)
