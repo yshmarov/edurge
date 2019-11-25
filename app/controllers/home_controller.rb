2cd35a326735400689f40acbfbdd2b3fcbb8d5b1
Yaro <yshmarov@gmail.com>
2019-11-25 10:58:39 +0000

    add reviews to home/index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:index]
   def index
     @latest_courses = Course.all.published.approved.order(created_at: :desc).limit(3)
+    @top_rated_courses = Course.all.published.approved.order(created_at: :desc).limit(3)
     @popular_categories = Category.all.order(courses_count: :desc).limit(3)
+    @latest_good_reviews = Review.all.order(rating: :desc, created_at: :desc).limit(3)
   end
 end
