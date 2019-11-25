c539cd7a668b9b44b2faeb9a7b67fb850d8dfda0
Yaro <yshmarov@gmail.com>
2019-11-25 16:43:25 +0000

    make reviews visible in course/show

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -49,6 +49,7 @@ class CoursesController < ApplicationController
 
   def show
     @lessons = @course.lessons.order(chapter: :asc)
+    @reviews = @course.reviews.order(created_at: :desc).limit(2)
   end
 
   def new
