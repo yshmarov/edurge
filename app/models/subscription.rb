4cd3aead7ab84e0c9c51324e2ccc624e418b097c
Yaro <yshmarov@gmail.com>
2019-11-28 12:27:50 +0000

    subscription validate :cant_subscribe_to_own_course

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,26 +2,26 @@ class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :course, counter_cache: true
 
+  scope :pending_review, -> { where(rating: [0, nil, ""], comment: [nil, ""]) }
+  scope :rated, -> { where.not(comment: [nil, ""]) }
+
   validates :user, :course, presence: true
 
   validates_presence_of :rating, if: :comment?
   validates_presence_of :comment, if: :rating?
 
   validates_uniqueness_of :user_id, scope: :course_id  #user cant be subscribed to the same course twice
-  validates_uniqueness_of :course_id, scope: :user_id
+  validates_uniqueness_of :course_id, scope: :user_id  #user cant be subscribed to the same course twice
   
-  #user can't create a subscription if course.user == current_user.id
-  #validate :cant_subscribe_to_own_course
-  #def cant_subscribe_to_own_course
-  #  if self.new_record?
-  #    if self.course == user.course
-  #      errors.add(:base, "You can not subscribe to your own course")
-  #    end
-  #  end
-  #end
-
-  scope :pending_review, -> { where(rating: [0, nil, ""], comment: [nil, ""]) }
-  scope :rated, -> { where.not(comment: [nil, ""]) }
+  validate :cant_subscribe_to_own_course  #user can't create a subscription if course.user == current_user.id
+  protected
+  def cant_subscribe_to_own_course
+    if self.new_record?
+      if user_id == course.user_id
+        errors.add(:base, "You can not subscribe to your own course")
+      end
+    end
+  end
 
   after_save do
     unless rating.nil? || rating.zero?
