c88d3baa7dba0a639a5cba796e47646a2adb4852
Yaro <yashm@outlook.com>
2019-12-06 21:31:14 +0000

    lesson can be deleted

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -53,7 +53,7 @@ class LessonsController < ApplicationController
   def destroy
     @lesson.destroy
     respond_to do |format|
-      format.html { redirect_to lessons_url, notice: 'Lesson was successfully destroyed.' }
+      format.html { redirect_to course_path(@course), notice: 'Lesson was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
