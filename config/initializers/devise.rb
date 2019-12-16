3a2df4ab16cf682aa1b0304405cb9a4c44c7653f
Yaro <yashm@outlook.com>
2019-12-16 11:53:45 +0000

    config.reconfirmable = false

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -117,10 +117,10 @@ Devise.setup do |config|
   # config.pepper = 'f91e7365ad360de1658087c42ea266ccae257fa355387c6c1043f0302b48e91db92da55b6ef1ca1ed472e2d74de688a272fd9ba17f82c609def79131937bfb46'
 
   # Send a notification to the original email when the user's email is changed.
-  # config.send_email_changed_notification = false
+  config.send_email_changed_notification = true
 
   # Send a notification email when the user's password is changed.
-  # config.send_password_change_notification = false
+  config.send_password_change_notification = true
 
   # ==> Configuration for :confirmable
   # A period that the user is allowed to access the website even without
@@ -145,7 +145,7 @@ Devise.setup do |config|
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
   # db field (see migrations). Until confirmed, new email is stored in
   # unconfirmed_email column, and copied to email column on successful confirmation.
-  config.reconfirmable = true
+  config.reconfirmable = false
 
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [:email]
